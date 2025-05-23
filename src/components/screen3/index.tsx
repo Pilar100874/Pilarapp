2d45f6514686d69ef061d6261e7f2c81f315bf4e
Pilar100874 <pilar@pilarapp.com.br>
2025-05-22 22:18:26 -0300

    Update Screen3 rotation

--- src/components/screen3/index.tsx
+++ src/components/screen3/index.tsx
@@ -6,7 +6,7 @@ import { Photo } from './photo';
 export const Screen3 = () => {
   return (
     <Scroll>
-      <group position-y={SCREEN3_OFFSET_START_Y} rotation-y={Math.PI * -0.05}>
+      <group position-y={SCREEN3_OFFSET_START_Y} rotation-y={Math.PI * 0.05}>
         {Object.entries(dataPhotos).map(([name, src], i) => (
           <Photo key={name} index={i} src={src} />
         ))}
