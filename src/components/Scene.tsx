b9692fa9421f335579a37cf276e5cd28952b337b
Pilar100874 <pilar@pilarapp.com.br>
2025-05-23 11:44:17 -0300

    Remove Screen 4

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -4,17 +4,15 @@ import { Color } from 'three';
 import { Opener } from '@/components/opener';
 import { Screen2 } from '@/components/screen2';
 import { Screen3 } from '@/components/screen3';
-import { Screen4 } from '@/components/screen4';
 
 export const Scene = () => {
   return (
     <Canvas style={{ width: '100vw', height: '100vh' }}>
-      <ScrollControls pages={6}>
+      <ScrollControls pages={5}>
         <color attach="background" args={[new Color('black')]} />
         <Opener />
         <Screen2 />
         <Screen3 />
-        <Screen4 />
       </ScrollControls>
       <ambientLight />
       <directionalLight />
