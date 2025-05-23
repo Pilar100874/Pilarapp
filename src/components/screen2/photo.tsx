62ce931a194a9e4cb76e5abe94a7ddea578c652b
Pilar100874 <pilar@pilarapp.com.br>
2025-05-22 22:03:39 -0300

    Fix photo flip animation in Screen2

--- src/components/screen2/photo.tsx
+++ src/components/screen2/photo.tsx
@@ -1,8 +1,7 @@
 import { Plane, useScroll, useTexture } from '@react-three/drei';
 import { useFrame, ThreeEvent } from '@react-three/fiber';
 import { useRef, useState } from 'react';
-import { MathUtils, Mesh } from 'three';
-import { easeOutQuart } from '../screen3/utils';
+import { MathUtils, Mesh, Group } from 'three';
 
 type Photo = {
   src: string;
@@ -10,42 +9,55 @@ type Photo = {
 };
 
 export const Photo = (props: Photo) => {
-  const photo = useTexture(props.src);
-  const ref = useRef<Mesh>(null);
-  const scroll = useScroll();
+  const frontTexture = useTexture(props.src);
+  const backTexture = useTexture(`/k-0${props.index + 1}.jpeg`);
+  const groupRef = useRef<Group>(null);
   const [isFlipped, setIsFlipped] = useState(false);
   const [zPosition, setZPosition] = useState(props.index * -0.35);
 
   const handleClick = (event: ThreeEvent<MouseEvent>) => {
     event.stopPropagation();
     setIsFlipped(!isFlipped);
-    setZPosition(isFlipped ? props.index * -0.35 : (5 - props.index) * -0.35);
+    setZPosition(isFlipped ? props.index * -0.35 : (5 - props.index) * 0.35);
   };
 
   useFrame(() => {
-    if (!ref.current) {
-      return;
-    }
+    if (!groupRef.current) return;
 
     // Smooth rotation animation for flip
     const targetRotation = isFlipped ? Math.PI : 0;
-    ref.current.rotation.y = MathUtils.lerp(ref.current.rotation.y, targetRotation, 0.1);
+    groupRef.current.rotation.y = MathUtils.lerp(groupRef.current.rotation.y, targetRotation, 0.1);
 
     // Smooth z-position transition
-    ref.current.position.z = MathUtils.lerp(ref.current.position.z, zPosition, 0.1);
+    groupRef.current.position.z = MathUtils.lerp(groupRef.current.position.z, zPosition, 0.1);
   });
 
   return (
-    <Plane
-      ref={ref}
-      onClick={handleClick}
+    <group
+      ref={groupRef}
       position-x={props.index % 2 === 0 ? -1.5 : 1.5}
       position-y={-0.25 + Math.random() * 0.5}
       position-z={props.index * -0.35}
-      args={[3.25, 4.5]}
-      material-map={photo}
-      material-transparent
-      material-alphaTest={0.1}
-    />
+      onClick={handleClick}
+    >
+      {/* Front of the photo */}
+      <Plane
+        args={[3.25, 4.5]}
+        position-z={0.001}
+        material-map={frontTexture}
+        material-transparent
+        material-alphaTest={0.1}
+      />
+      
+      {/* Back of the photo */}
+      <Plane
+        args={[3.25, 4.5]}
+        rotation-y={Math.PI}
+        position-z={-0.001}
+        material-map={backTexture}
+        material-transparent
+        material-alphaTest={0.1}
+      />
+    </group>
   );
 };
