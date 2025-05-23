bce8bc3b743bfb3293f403b9a63e16dea56d1df2
Pilar100874 <pilar@pilarapp.com.br>
2025-05-22 22:07:22 -0300

    Update screens layout

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -3,17 +3,18 @@ import { Canvas } from '@react-three/fiber';
 import { Color } from 'three';
 import { Opener } from '@/components/opener';
 import { Screen2 } from '@/components/screen2';
-import { Screen3 } from '@/components/screen3';
 import { Screen4 } from '@/components/screen4';
 
 export const Scene = () => {
   return (
     <Canvas style={{ width: '100vw', height: '100vh' }}>
-      <ScrollControls pages={6}>
+      <ScrollControls pages={5}>
         <color attach="background" args={[new Color('black')]} />
         <Opener />
         <Screen2 />
-        <Screen3 />
+        <group position-y={-16}>
+          <Opener />
+        </group>
         <Screen4 />
       </ScrollControls>
       <ambientLight />
