4d7fef6d897f43077b6fdf66b22f750472110863
Pilar100874 <pilar@pilarapp.com.br>
2025-05-23 12:05:13 -0300

    Create Screen6 as a copy of Screen3

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -5,16 +5,18 @@ import { Opener } from '@/components/opener';
 import { Screen2 } from '@/components/screen2';
 import { Screen3 } from '@/components/screen3';
 import { Screen4 } from '@/components/screen4';
+import { Screen6 } from '@/components/screen6';
 
 export const Scene = () => {
   return (
     <Canvas style={{ width: '100vw', height: '100vh' }}>
-      <ScrollControls pages={6}>
+      <ScrollControls pages={7}>
         <color attach="background" args={[new Color('black')]} />
         <Opener />
         <Screen2 />
         <Screen3 />
         <Screen4 />
+        <Screen6 />
       </ScrollControls>
       <ambientLight />
       <directionalLight />
---
+++ src/components/screen6/constants.tsx
@@ -0,0 +1,2 @@
+export const SCREEN6_OFFSET_START_Y = -33.5;
+export const PHOTO_OFFSET_FACTOR = 0.5;
