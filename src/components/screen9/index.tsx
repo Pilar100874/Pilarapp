8441031b84e7e5242f3acfb8361b5101389f2342
Pilar100874 <pilar@pilarapp.com.br>
2025-05-24 16:56:17 -0300

    Add contact information screen

--- src/components/screen9/index.tsx
+++ src/components/screen9/index.tsx
@@ -4,18 +4,31 @@ import { SCREEN9_OFFSET_START_Y } from './constants';
 export const Screen9 = () => {
   return (
     <Scroll>
-      <Text
-        fontSize={2}
-        letterSpacing={0.005}
-        position-y={SCREEN9_OFFSET_START_Y}
-        position-z={0.1}
-        font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-        anchorX="center"
-        anchorY="middle"
-      >
-        Pilar Pap
-        <meshBasicMaterial depthTest={false} />
-      </Text>
+      <group position-y={SCREEN9_OFFSET_START_Y}>
+        <Text
+          fontSize={0.5}
+          letterSpacing={0.005}
+          position-z={0.1}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle"
+        >
+          Endereço - Matriz: Rua Jardim Suspenso 126 - EMBU SP
+          <meshBasicMaterial depthTest={false} />
+        </Text>
+        <Text
+          fontSize={0.5}
+          letterSpacing={0.005}
+          position-z={0.1}
+          position-y={-0.75}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle"
+        >
+          Tel (11) 2135-4444
+          <meshBasicMaterial depthTest={false} />
+        </Text>
+      </group>
     </Scroll>
   );
 };
