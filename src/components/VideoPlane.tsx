d51bc26c2ef8121cdb52cea7c5a00dcf7d3bcf3f
Pilar100874 <pilar@pilarapp.com.br>
2025-05-24 15:29:08 -0300

    Fix missing image reference

--- src/components/VideoPlane.tsx
+++ src/components/VideoPlane.tsx
@@ -3,7 +3,6 @@ import {
   useAspect,
   useScroll,
   useVideoTexture,
-  useTexture,
 } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
@@ -13,11 +12,9 @@ import { OpenerText } from "@/components/opener/openerText";
 type VideoPlane = {
   texturePath: string;
 };
-
 export const VideoPlane = ({ texturePath }: VideoPlane) => {
   const scroll = useScroll();
   const windowSize = useAspect(1920, 1080);
-  const logoTexture = useTexture('/spots.png');
 
   const videoTexture = useVideoTexture(texturePath, {
     autoplay: true,
@@ -40,12 +37,6 @@ export const VideoPlane = ({ texturePath }: VideoPlane) => {
         scale={[...windowSize]}
         material-side={DoubleSide}
         material-map={videoTexture}
-      />
-      <Plane
-        scale={[2, 1]}
-        position={[-4, 2, 0.1]}
-        material-transparent
-        material-map={logoTexture}
       />
       <OpenerText py={0.5} />
     </Suspense>
