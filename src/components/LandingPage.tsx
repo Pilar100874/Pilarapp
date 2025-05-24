753ffe3f216c921d4e0ec9a69b7212056e968600
Pilar100874 <pilar@pilarapp.com.br>
2025-05-24 15:08:59 -0300

    Reduce button height

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -28,7 +28,7 @@ export const LandingPage = ({ onStart }: { onStart: () => void }) => {
         onPointerEnter={() => setIsHovered(true)}
         onPointerLeave={() => setIsHovered(false)}
       >
-        <RoundedBox args={[2.4, 0.6, 0.1]} radius={0.3} smoothness={16}>
+        <RoundedBox args={[2.4, 0.5, 0.1]} radius={0.3} smoothness={16}>
           <meshBasicMaterial color="white" />
         </RoundedBox>
         <Text
