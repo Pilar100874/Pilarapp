59a71df3ac5eb1387ed3418f8b56a2a2c8ed2585
Pilar100874 <pilar@pilarapp.com.br>
2025-05-24 16:46:23 -0300

    Add final screen with Pilar Pap text

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,11 +8,12 @@ import { Screen4 } from '@/components/screen4';
 import { Screen6 } from '@/components/screen6';
 import { Screen7 } from '@/components/screen7';
 import { Screen8 } from '@/components/screen8';
+import { Screen9 } from '@/components/screen9';
 
 export const Scene = () => {
   return (
     <Canvas style={{ width: '100vw', height: '100vh' }}>
-      <ScrollControls pages={9}>
+      <ScrollControls pages={10}>
         <color attach="background" args={[new Color('black')]} />
         <Opener />
         <Screen2 />
@@ -21,6 +22,7 @@ export const Scene = () => {
         <Screen6 />
         <Screen7 />
         <Screen8 />
+        <Screen9 />
       </ScrollControls>
       <ambientLight />
       <directionalLight />
