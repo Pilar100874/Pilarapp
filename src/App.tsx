ac4628f4fab9377fe768191774a28cbdded4be28
Pilar100874 <pilar@pilarapp.com.br>
2025-05-25 08:57:42 -0300

    Add video transition animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import { useState } from 'react';
 import { Scene } from '@/components/Scene';
 import { LandingScene } from '@/components/LandingScene';
-import { TransitionVideo } from '@/components/TransitionVideo';
 
 function App() {
   const [started, setStarted] = useState(false);
-  const [transitionComplete, setTransitionComplete] = useState(false);
 
-  const handleStart = () => {
-    setStarted(true);
-  };
-
-  return transitionComplete ? (
-    <Scene />
-  ) : started ? (
-    <TransitionVideo onTransitionComplete={() => setTransitionComplete(true)} />
-  ) : (
-    <LandingScene onStart={handleStart} />
-  );
+  return started ? <Scene /> : <LandingScene onStart={() => setStarted(true)} />;
 }
 
 export default App;
