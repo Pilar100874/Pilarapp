fca7b18cfd870f1308f7f6108ee5b07407b20645
Pilar100874 <pilar@pilarapp.com.br>
2025-05-25 14:07:22 -0300

    Create data file for Screen5

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -5,7 +5,6 @@ import { Opener } from '@/components/opener';
 import { Screen2 } from '@/components/screen2';
 import { Screen3 } from '@/components/screen3';
 import { Screen4 } from '@/components/screen4';
-import { Screen5 } from '@/components/screen5';
 import { Screen6 } from '@/components/screen6';
 import { Screen7 } from '@/components/screen7';
 import { Screen8 } from '@/components/screen8';
@@ -14,7 +13,7 @@ export const Scene = () => {
   return (
     <Canvas style={{ width: '100vw', height: '100vh' }}>
       <color attach="background" args={[new Color('black')]} />
-      <ScrollControls pages={9} damping={0.35} distance={0.25}>
+      <ScrollControls pages={8} damping={0.35} distance={0.25}>
         <Opener />
         <Screen2 />
         <Screen3 />
@@ -22,7 +21,6 @@ export const Scene = () => {
         <Screen6 />
         <Screen7 />
         <Screen8 />
-        <Screen5 />
       </ScrollControls>
       <ambientLight />
       <directionalLight />
