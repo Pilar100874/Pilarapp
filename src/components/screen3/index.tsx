3eee8176559fef376d1c825b72459f62a76ffbe5
Pilar100874 <pilar@pilarapp.com.br>
2025-05-25 17:09:52 -0300

    Add scroll snap to screen3

---
+++ src/components/screen3/index.tsx
@@ -0,0 +1,32 @@
+import { Scroll } from '@react-three/drei';
+import { SCREEN3_OFFSET_START_Y } from './constants';
+import { dataPhotos } from './dataPhotos';
+import { Photo } from './photo';
+import { useState } from 'react';
+
+export const Screen3 = () => {
+  const photoList = Object.entries(dataPhotos);
+  const [activeIndex, setActiveIndex] = useState(Math.floor(photoList.length / 2));
+
+  const handlePhotoClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <Scroll>
+      <group position-y={SCREEN3_OFFSET_START_Y}>
+        {photoList.map(([name, src], index) => (
+          <Photo
+            key={name}
+            src={src}
+            index={index}
+            isActive={index === activeIndex}
+            totalPhotos={photoList.length}
+            activeIndex={activeIndex}
+            onClick={() => handlePhotoClick(index)}
+          />
+        ))}
+      </group>
+    </Scroll>
+  );
+};
