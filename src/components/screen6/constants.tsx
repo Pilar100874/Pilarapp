e7db473366fe0ae2cc7765477e2198e9f427bde9
Pilar100874 <pilar@pilarapp.com.br>
2025-05-25 15:46:35 -0300

    Move Screen6 up by 1cm

--- src/components/screen6/constants.tsx
+++ src/components/screen6/constants.tsx
@@ -1,2 +1,2 @@
-export const SCREEN6_OFFSET_START_Y = -41.5; // Moved up by 1 unit (1 cm)
+export const SCREEN6_OFFSET_START_Y = -26.5; // Moved up by 1 unit (1 cm)
 export const PHOTO_OFFSET_FACTOR = 0.5;
