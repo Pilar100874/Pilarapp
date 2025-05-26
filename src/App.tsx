33bd0136a0d181b1289143024b94d3852c903632
Pilar100874 <pilar@pilarapp.com.br>
2025-05-26 13:20:53 -0300

    Update application for fullscreen and responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   const handleStart = async () => {
     try {
-      // Request fullscreen on start
+      // Try to request fullscreen on start
       if (document.documentElement.requestFullscreen) {
         await document.documentElement.requestFullscreen();
       } else if ((document.documentElement as any).webkitRequestFullscreen) {
@@ -20,6 +20,24 @@ function App() {
     }
     setStarted(true);
   };
+
+  // Request fullscreen immediately on component mount
+  useState(() => {
+    const requestFullscreen = async () => {
+      try {
+        if (document.documentElement.requestFullscreen) {
+          await document.documentElement.requestFullscreen();
+        } else if ((document.documentElement as any).webkitRequestFullscreen) {
+          await (document.documentElement as any).webkitRequestFullscreen();
+        } else if ((document.documentElement as any).msRequestFullscreen) {
+          await (document.documentElement as any).msRequestFullscreen();
+        }
+      } catch (error) {
+        console.warn('Initial fullscreen request failed:', error);
+      }
+    };
+    requestFullscreen();
+  }, []);
 
   return started ? <Scene /> : <LandingScene onStart={handleStart} />;
 }
