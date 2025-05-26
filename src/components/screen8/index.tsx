b6f9763c977ecb5fc0401f3b04e46d0653d3c9d2
Pilar100874 <pilar@pilarapp.com.br>
2025-05-26 12:38:50 -0300

    Make Screen8 responsive

--- src/components/screen8/index.tsx
+++ src/components/screen8/index.tsx
@@ -2,19 +2,14 @@ import { Scroll } from '@react-three/drei';
 import { SCREEN8_OFFSET_START_Y } from './constants';
 import { dataPhotos } from './dataPhotos';
 import { Photo } from './photo';
-import { useThree } from '@react-three/fiber';
+import { useState } from 'react';
 
 export const Screen8 = () => {
   const photoList = Object.entries(dataPhotos);
-  const { viewport } = useThree();
-  const isMobile = viewport.width < 5;
-
-  // Adjust vertical offset for mobile
-  const verticalOffset = isMobile ? SCREEN8_OFFSET_START_Y * 0.8 : SCREEN8_OFFSET_START_Y;
 
   return (
     <Scroll>
-      <group position-y={verticalOffset}>
+      <group position-y={SCREEN8_OFFSET_START_Y}>
         {photoList.map(([name, src], index) => (
           <Photo
             key={name}
