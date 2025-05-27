b1dd293324cc9a7eb50d073a11c3526815216fd3
Pilar100874 <pilar@pilarapp.com.br>
2025-05-27 19:12:51 -0300

    Move Screen7 up by 4cm

--- src/components/screen7/VideoPlane.tsx
+++ src/components/screen7/VideoPlane.tsx
@@ -33,9 +33,9 @@ export const VideoPlane = ({ texturePath }: VideoPlane) => {
     ref.current.rotation.y = scroll.offset * 2.5;
   });
 
-  // Move up by 2cm (20 units)
-  const baseY = -58.5 + 2;
-  const textY = -58 + 2;
+  // Move up by 4cm (40 units)
+  const baseY = -58.5 + 4;
+  const textY = -58 + 4;
 
   return (
     <Suspense fallback={null}>
