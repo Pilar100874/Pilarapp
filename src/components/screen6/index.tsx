d80485d3228a5a894d419b15b2d53c209785d7cd
Pilar100874 <pilar@pilarapp.com.br>
2025-05-27 16:14:32 -0300

    Add navigation buttons to Screen6

--- src/components/screen6/index.tsx
+++ src/components/screen6/index.tsx
@@ -3,60 +3,25 @@ import { SCREEN6_OFFSET_START_Y } from './constants';
 import { dataPhotos } from './dataPhotos';
 import { Photo } from './photo';
 import { useState } from 'react';
-import { useTexture } from '@react-three/drei';
 
 export const Screen6 = () => {
   const photoList = Object.entries(dataPhotos);
   const [order, setOrder] = useState(photoList.map((_, i) => i));
-  const arrowTexture = useTexture('/seta_B.png');
 
-  const handlePrevious = () => {
+  const rotatePhotos = (clickedIndex: number) => {
     const newOrder = [...order];
-    const last = newOrder.pop();
-    if (last !== undefined) {
-      newOrder.unshift(last);
-      setOrder(newOrder);
-    }
-  };
-
-  const handleNext = () => {
-    const newOrder = [...order];
-    const first = newOrder.shift();
-    if (first !== undefined) {
-      newOrder.push(first);
-      setOrder(newOrder);
-    }
+    const currentPosition = order.indexOf(clickedIndex);
+    
+    // Move clicked photo to front by rotating array
+    const itemsToRotate = newOrder.splice(currentPosition);
+    newOrder.unshift(...itemsToRotate);
+    
+    setOrder(newOrder);
   };
 
   return (
     <Scroll>
       <group position-y={SCREEN6_OFFSET_START_Y} rotation-y={Math.PI * -0.05}>
-        {/* Previous Button */}
-        <mesh
-          position={[-5, 0, 0]}
-          scale={[0.5, 0.5, 1]}
-          rotation={[0, 0, Math.PI / 2]}
-          onClick={handlePrevious}
-          onPointerOver={() => { document.body.style.cursor = 'pointer'; }}
-          onPointerOut={() => { document.body.style.cursor = 'default'; }}
-        >
-          <planeGeometry args={[1, 1]} />
-          <meshBasicMaterial map={arrowTexture} transparent opacity={1} />
-        </mesh>
-
-        {/* Next Button */}
-        <mesh
-          position={[5, 0, 0]}
-          scale={[0.5, 0.5, 1]}
-          rotation={[0, 0, -Math.PI / 2]}
-          onClick={handleNext}
-          onPointerOver={() => { document.body.style.cursor = 'pointer'; }}
-          onPointerOut={() => { document.body.style.cursor = 'default'; }}
-        >
-          <planeGeometry args={[1, 1]} />
-          <meshBasicMaterial map={arrowTexture} transparent opacity={1} />
-        </mesh>
-
         {order.map((originalIndex, displayIndex) => {
           const [name, src] = photoList[originalIndex];
           return (
@@ -66,13 +31,7 @@ export const Screen6 = () => {
               z={-displayIndex * 0.35}
               index={displayIndex}
               totalPhotos={photoList.length}
-              onClick={() => {
-                const newOrder = [...order];
-                const currentPosition = displayIndex;
-                const itemsToRotate = newOrder.splice(currentPosition);
-                newOrder.unshift(...itemsToRotate);
-                setOrder(newOrder);
-              }}
+              onClick={() => rotatePhotos(originalIndex)}
             />
           );
         })}
