7a16066cc1d71689395c5c7dd92637232041e80e
Pilar100874 <pilar@pilarapp.com.br>
2025-05-27 17:11:17 -0300

    Fix ShapeGeometry implementation

--- src/components/screen6/index.tsx
+++ src/components/screen6/index.tsx
@@ -3,6 +3,7 @@ import { SCREEN6_OFFSET_START_Y } from './constants';
 import { dataPhotos } from './dataPhotos';
 import { Photo } from './photo';
 import { useState } from 'react';
+import { Shape } from 'three';
 
 export const Screen6 = () => {
   const photoList = Object.entries(dataPhotos);
@@ -19,6 +20,34 @@ export const Screen6 = () => {
     
     setOrder(newOrder);
   };
+
+  // Create Shape objects for play and pause icons
+  const createShape = (points: { x: number; y: number }[]) => {
+    const shape = new Shape();
+    shape.moveTo(points[0].x, points[0].y);
+    for (let i = 1; i < points.length; i++) {
+      shape.lineTo(points[i].x, points[i].y);
+    }
+    shape.lineTo(points[0].x, points[0].y); // Close the shape
+    return shape;
+  };
+
+  const playShape = createShape([
+    { x: -0.15, y: -0.15 },
+    { x: -0.15, y: 0.15 },
+    { x: 0.15, y: 0 }
+  ]);
+
+  const pauseShape = createShape([
+    { x: -0.15, y: -0.15 },
+    { x: -0.15, y: 0.15 },
+    { x: -0.05, y: 0.15 },
+    { x: -0.05, y: -0.15 },
+    { x: 0.05, y: -0.15 },
+    { x: 0.05, y: 0.15 },
+    { x: 0.15, y: 0.15 },
+    { x: 0.15, y: -0.15 }
+  ]);
 
   return (
     <Scroll>
@@ -46,26 +75,8 @@ export const Screen6 = () => {
             onPointerOut={() => { document.body.style.cursor = 'default'; }}
           >
             <planeGeometry args={[0.5, 0.5]} />
-            <meshBasicMaterial color="white" transparent>
-              <shapeGeometry args={[
-                isAnimationPaused 
-                  ? [
-                      { x: -0.15, y: -0.15 },
-                      { x: -0.15, y: 0.15 },
-                      { x: 0.15, y: 0 }
-                    ] // Play triangle
-                  : [
-                      { x: -0.15, y: -0.15 },
-                      { x: -0.15, y: 0.15 },
-                      { x: -0.05, y: 0.15 },
-                      { x: -0.05, y: -0.15 },
-                      { x: 0.05, y: -0.15 },
-                      { x: 0.05, y: 0.15 },
-                      { x: 0.15, y: 0.15 },
-                      { x: 0.15, y: -0.15 }
-                    ] // Pause bars
-              ]} />
-            </meshBasicMaterial>
+            <meshBasicMaterial color="white" transparent opacity={0} />
+            <shapeGeometry args={[isAnimationPaused ? playShape : pauseShape]} />
           </mesh>
         </group>
       </group>
