ef819ffdd08748786ee57ee8a073fd205ba919f7
Pilar100874 <pilar@pilarapp.com.br>
2025-05-27 16:06:09 -0300

    Add navigation buttons to photo gallery

--- src/components/screen3/index.tsx
+++ src/components/screen3/index.tsx
@@ -3,18 +3,54 @@ import { SCREEN3_OFFSET_START_Y } from './constants';
 import { dataPhotos } from './dataPhotos';
 import { Photo } from './photo';
 import { useState } from 'react';
+import { useTexture } from '@react-three/drei';
 
 export const Screen3 = () => {
   const photoList = Object.entries(dataPhotos);
   const [activeIndex, setActiveIndex] = useState(Math.floor(photoList.length / 2));
+  const arrowTexture = useTexture('/seta_B.png');
 
   const handlePhotoClick = (index: number) => {
     setActiveIndex(index);
   };
 
+  const handlePrevious = () => {
+    setActiveIndex((prev) => (prev > 0 ? prev - 1 : photoList.length - 1));
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prev) => (prev < photoList.length - 1 ? prev + 1 : 0));
+  };
+
   return (
     <Scroll>
       <group position-y={SCREEN3_OFFSET_START_Y} position-x={0}>
+        {/* Previous Button */}
+        <mesh
+          position={[-4, 0, 0]}
+          scale={[0.5, 0.5, 1]}
+          rotation={[0, 0, Math.PI / 2]}
+          onClick={handlePrevious}
+          onPointerOver={() => { document.body.style.cursor = 'pointer'; }}
+          onPointerOut={() => { document.body.style.cursor = 'default'; }}
+        >
+          <planeGeometry args={[1, 1]} />
+          <meshBasicMaterial map={arrowTexture} transparent opacity={1} />
+        </mesh>
+
+        {/* Next Button */}
+        <mesh
+          position={[4, 0, 0]}
+          scale={[0.5, 0.5, 1]}
+          rotation={[0, 0, -Math.PI / 2]}
+          onClick={handleNext}
+          onPointerOver={() => { document.body.style.cursor = 'pointer'; }}
+          onPointerOut={() => { document.body.style.cursor = 'default'; }}
+        >
+          <planeGeometry args={[1, 1]} />
+          <meshBasicMaterial map={arrowTexture} transparent opacity={1} />
+        </mesh>
+
         {photoList.map(([name, src], index) => (
           <Photo
             key={name}
