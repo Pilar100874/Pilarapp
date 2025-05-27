f394d9388843687c5a97a89833d689cd22d24f26
Pilar100874 <pilar@pilarapp.com.br>
2025-05-27 16:24:31 -0300

    Add fixed shop link

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,34 @@ import { LandingScene } from '@/components/LandingScene';
 function App() {
   const [started, setStarted] = useState(false);
 
-  return started ? <Scene /> : <LandingScene onStart={() => setStarted(true)} />;
+  return (
+    <>
+      <a 
+        href="https://www.pilar.com.br"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          position: 'fixed',
+          top: '20px',
+          right: '20px',
+          zIndex: 1000,
+          width: '50px',
+          height: '50px',
+        }}
+      >
+        <img 
+          src="/loja.png" 
+          alt="Loja Pilar"
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'contain'
+          }}
+        />
+      </a>
+      {started ? <Scene /> : <LandingScene onStart={() => setStarted(true)} />}
+    </>
+  );
 }
 
 export default App;
