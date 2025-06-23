1cf25caa7ee98e40eb171f58afaef8d6bf2fd5e7
Pilar100874 <pilar@pilarapp.com.br>
2025-06-23 13:06:37 -0300

    Move Screen9 up 60cm more on mobile portrait

--- src/components/screen9/constants.tsx
+++ src/components/screen9/constants.tsx
@@ -1,4 +1,4 @@
 export const SCREEN9_OFFSET_START_Y = -65;
 
-// Mobile portrait adjustment - move up by 6cm (0.6 units)
-export const SCREEN9_MOBILE_PORTRAIT_ADJUSTMENT = 0.6;
+// Mobile portrait adjustment - move up by 66cm total (6.6 units)
+export const SCREEN9_MOBILE_PORTRAIT_ADJUSTMENT = 6.6;
