d674fce4e200463160afe46ca5588eed0dcac4af
Pilar100874 <pilar@pilarapp.com.br>
2025-06-23 13:11:47 -0300

    Move Screen11 up 120cm for mobile portrait

--- src/components/screen11/constants.tsx
+++ src/components/screen11/constants.tsx
@@ -1,1 +1,4 @@
 export const SCREEN11_OFFSET_START_Y = -70;
+
+// Mobile portrait adjustment - move up by 120cm (12 units)
+export const SCREEN11_MOBILE_PORTRAIT_ADJUSTMENT = 12;
--- src/components/screen11/index.tsx
+++ src/components/screen11/index.tsx
@@ -1,11 +1,19 @@
 import { Scroll } from '@react-three/drei';
-import { SCREEN11_OFFSET_START_Y } from './constants';
+import { SCREEN11_OFFSET_START_Y, SCREEN11_MOBILE_PORTRAIT_ADJUSTMENT } from './constants';
 import { ShopButton } from './ShopButton';
+import { useResponsiveText } from '@/utils/responsive';
 
 export const Screen11 = () => {
+  const { isMobilePortrait } = useResponsiveText();
+  
+  // Adjust position for mobile portrait - move up by 120cm
+  const adjustedY = isMobilePortrait 
+    ? SCREEN11_OFFSET_START_Y + SCREEN11_MOBILE_PORTRAIT_ADJUSTMENT 
+    : SCREEN11_OFFSET_START_Y;
+
   return (
     <Scroll>
-      <group position-y={SCREEN11_OFFSET_START_Y}>
+      <group position-y={adjustedY}>
         <ShopButton />
       </group>
     </Scroll>
