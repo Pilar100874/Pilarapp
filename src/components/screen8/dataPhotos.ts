cc42ca7d988e2ed423e4e09050258ce24f047d4b
Pilar100874 <pilar@pilarapp.com.br>
2025-06-23 10:25:57 -0300

    Updated dataPhotos.ts

--- src/components/screen8/dataPhotos.ts
+++ src/components/screen8/dataPhotos.ts
@@ -9,7 +9,7 @@ export const dataPhotos: DataPhotos = {
   Photo1: { default: '/k-01.png', alternate: '/k-01b.png' },
   Photo2: { default: '/k-02.png', alternate: '/k-02b.png' },
   Photo3: { default: '/k-03.png', alternate: '/k-03b.png' },
-  Photo4: { default: '/k-04.jpeg', alternate: '/k-04b.png' },
+  Photo4: { default: '/k-04.png', alternate: '/k-04b.png' },
   Photo5: { default: '/k-05.png', alternate: '/k-05b.png' },
   Photo6: { default: '/k-06.png', alternate: '/k-06b.png' },
 };
