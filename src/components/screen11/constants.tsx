0a9eb341d617a228659ecaf66f0777ca987d6faf
Pilar100874 <pilar@pilarapp.com.br>
2025-06-23 13:13:28 -0300

    Move Screen11 down 40cm in mobile portrait

--- src/components/screen11/constants.tsx
+++ src/components/screen11/constants.tsx
@@ -1,4 +1,4 @@
 export const SCREEN11_OFFSET_START_Y = -70;
 
-// Mobile portrait adjustment - move up by 120cm (12 units)
-export const SCREEN11_MOBILE_PORTRAIT_ADJUSTMENT = 12;
+// Mobile portrait adjustment - move up by 80cm (8 units) - reduced by 40cm from previous 120cm
+export const SCREEN11_MOBILE_PORTRAIT_ADJUSTMENT = 8;
