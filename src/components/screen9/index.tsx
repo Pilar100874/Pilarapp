33f0d6e9891acb573ef2cb97dbdc75f04743be04
Pilar100874 <pilar@pilarapp.com.br>
2025-06-23 10:56:24 -0300

    Create shop button after screen9

--- src/components/screen9/index.tsx
+++ src/components/screen9/index.tsx
@@ -2,6 +2,7 @@ import { Scroll } from '@react-three/drei';
 import { SCREEN9_OFFSET_START_Y } from './constants';
 import { dataScreen9 } from './data';
 import { Word } from './word';
+import { ShopButton } from './ShopButton';
 
 export const Screen9 = () => {
   return (
@@ -9,6 +10,7 @@ export const Screen9 = () => {
       {dataScreen9.map((word, i) => (
         <Word key={word} value={word} index={dataScreen9.length - i} />
       ))}
+      <ShopButton />
     </Scroll>
   );
 };
