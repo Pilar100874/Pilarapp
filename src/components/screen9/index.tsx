be4744aa823939861a1fed3ec5919e13cf155e4b
Pilar100874 <pilar@pilarapp.com.br>
2025-06-23 10:57:53 -0300

    Updated index.tsx

--- src/components/screen9/index.tsx
+++ src/components/screen9/index.tsx
@@ -2,7 +2,6 @@ import { Scroll } from '@react-three/drei';
 import { SCREEN9_OFFSET_START_Y } from './constants';
 import { dataScreen9 } from './data';
 import { Word } from './word';
-import { ShopButton } from './ShopButton';
 
 export const Screen9 = () => {
   return (
@@ -10,7 +9,6 @@ export const Screen9 = () => {
       {dataScreen9.map((word, i) => (
         <Word key={word} value={word} index={dataScreen9.length - i} />
       ))}
-      <ShopButton />
     </Scroll>
   );
 };
